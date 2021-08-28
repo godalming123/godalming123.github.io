6d76d24571c787eee6b17a2d0c724187830e0863
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-08-28 09:04:07 -0400

    use barba

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
-const swup = new Swup();
+barba.init({
+  transitions: [{
+    name: 'opacity-transition',
+    leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 0
+      });
+    },
+    enter(data) {
+      return gsap.from(data.next.container, {
+        opacity: 0
+      });
+    }
+  }]
+});
 
 //Setup Google Analytics
 const doNotTrackOption = (
