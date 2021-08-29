7160522c99475bd7d4c1e3d34f1b1661a92b379e
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-08-29 04:40:26 -0400

    test new animations

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,31 +1,35 @@
+class animation {
+    //takes one property animationProps
+    //this is just a mapping of gsap props eg:
+    //{opacity: 0, x: 100}
+    //this has to functions leave and enter they both take a variable animation item and return
+    //gsap.to/from(anamationItem, this.animationProps) where animations props is the propertys you specify for animation
+    constructor(animationProps) {
+        this.animationProps = animationProps;
+    }
+
+    enter(animationItem) {
+        return gsap.from(animationItem, this.animationProps);
+    }
+
+    leave(animationItem) {
+        return gsap.to  (animationItem, this.animationProps);
+    }
+}
+
+const sidebarAnimation = new animation({
+  opacity: 0,
+  x: -100
+})
+
+const pageItemsAnimation = new animation({
+    opacity: 0,
+    y: 100,
+})
+
 barba.init({
   transitions: [{
-    name: 'opacity-transition',
-    leave: (data) => pageContentLeaveAnimation(data),
-    enter: (data) => pageContentLoadAnimation(data),
+    leave: (data) => pageItemsAnimation.leave(data.current.container),
+    enter: (data) => pageContentLoadAnimation.enter(data.next.container),
   }]
 });
-
-//content
-function pageContentLoadAnimation(data, enter: "right") {//enter allows you to make content enter from right or left
-  return gsap.from(data.next.container, {
-    opacity: 0,
-    duration: 0.1
-  });
-}
-
-function pageContentLeaveAnimation(data, leave: "left") {//leave allows you to make content leave from left or right
-  return gsap.to(data.current.container, {
-    opacity: 0,
-    duration: 0.1
-  });
-}
-
-//sidebar
-function pageSidebarLoadAnimation() {
-
-}
-
-function pageSidebarLeaveAnimation() {
-
-}
