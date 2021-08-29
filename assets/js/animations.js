603c655dadc8d87427fd04cf88d40bac06947e9f
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-08-29 03:39:40 -0400

    refactor

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,27 +1,24 @@
 barba.init({
-    transitions: [{
-      name: 'opacity-transition',
-      leave(data) {
-        return gsap.to(data.current.container, {
-          opacity: 0
-        });
-      },
-      enter(data) {
-        return gsap.from(data.next.container, {
-          opacity: 0,
-          duration: 0.5
-        });
-      }
-    }]
-  });
+  transitions: [{
+    name: 'opacity-transition',
+    leave: (data) => pageContentLeaveAnimation(),
+    enter: (data) => pageContentLoadAnimation(),
+  }]
+});
 
 //content
 function pageContentLoadAnimation() {
-
+  return gsap.from(data.next.container, {
+    opacity: 0,
+    duration: 0.1
+  });
 }
 
 function pageContentLeaveAnimation() {
-
+  return gsap.to(data.current.container, {
+    opacity: 0
+    duration: 0.1
+  });
 }
 
 //sidebar
