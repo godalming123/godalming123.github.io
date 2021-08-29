9c02679e88098d4123709d827d1f533b26c3fa66
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-08-29 03:50:46 -0400

    fix bugs

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,22 +1,22 @@
 barba.init({
   transitions: [{
     name: 'opacity-transition',
-    leave: (data) => pageContentLeaveAnimation(),
-    enter: (data) => pageContentLoadAnimation(),
+    leave: (data) => pageContentLeaveAnimation(data),
+    enter: (data) => pageContentLoadAnimation(data),
   }]
 });
 
 //content
-function pageContentLoadAnimation() {
+function pageContentLoadAnimation(data, enter: "right") {//enter allows you to make content enter from right or left
   return gsap.from(data.next.container, {
     opacity: 0,
     duration: 0.1
   });
 }
 
-function pageContentLeaveAnimation() {
+function pageContentLeaveAnimation(data, leave: "left") {//leave allows you to make content leave from left or right
   return gsap.to(data.current.container, {
-    opacity: 0
+    opacity: 0,
     duration: 0.1
   });
 }
