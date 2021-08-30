f3003293fc8d08afc5e3dd855163bc6891b931cc
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-08-30 05:58:52 -0400

    fix bugs

--- assets/js/animations/pageFirstLoad.js
+++ assets/js/animations/pageFirstLoad.js
@@ -1,19 +1,20 @@
 //page first load animations
-const sidebarAnimation = new animation({
-  opacity: 0,
-  x: -100,
-  duration: 0.4,
-})
-  
-const pageItemsFirstLoadAnimation = new animation({
-  opacity: 0,
-  x: 100,
-  duration: 0.4,
-})
 
 function pageFirstLoad() {
-    sidebarAnimation.enter("header");
-    pageItemsFirstLoadAnimation.enter(".wrapper > section")
+  const sidebarAnimation = {
+    opacity: 0,
+    x: -100,
+    duration: 0.4,
+  }
+
+  const pageItemsFirstLoadAnimation = {
+    opacity: 0,
+    x: 100,
+    duration: 0.4,
+  }
+
+  gsap.from("header", sidebarAnimation)
+  gsap.from(".wrapper > section", pageItemsFirstLoadAnimation)
 }
 
 pageFirstLoad()
