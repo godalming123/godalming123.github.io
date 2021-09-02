94b68270760133db6cb2d446c8d972a40fb6678c
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-09-02 07:41:55 -0400

    update chached file in service worker

--- sw.js
+++ sw.js
@@ -1,43 +1,55 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    // after the service worker is installed,
-    // open a new cache
-    caches.open('my-pwa-cache').then(cache => {
-      // add all URLs of resources we want to cache
-      return cache.addAll([
-        //pages
-        '/',
-        '/index.html',
-        '/working_on.html',
-        '/blogs.html',
-        '/tuturiols.html',
-        //profile pictures
-        '/profile-pic/180.jpeg',
-        '/profile-pic/266.webp',
-        '/profile-pic/266.png',
-        //icons
-        '/icons/blogger.svg',
-        '/icons/hello.svg',
-        '/icons/home.svg',
-        '/icons/suitcase.svg',
-        //styles
-        '/assets/css/style.css',
-        '/assets/css/colors-light.css',
-        '/assets/css/colors-auto.css',
-        '/assets/css/colors-dark.css',
-        //js
-        '/assets/js/google-anylytics.js',
-        '/assets/js/animations/barba.js',
-        '/assets/js/animations/pageFirstLoad.js',
-        '/assets/js/animations/SlideOnVeiwportEnter.js',
-        '/sw.js',
-        //js dependencys
-        'https://unpkg.com/@barba/core',
-        'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/ScrollTrigger.min.js',
-        //other
-        '/assets/manifest.json'
-      ]);
-    })
+const chacheName = "pwa-chache";
+
+const assets = [
+  //pages
+  '/',
+  '/index.html',
+  '/working_on.html',
+  '/blogs.html',
+  '/tuturiols.html',
+  //profile pictures
+  '/profile-pic/180.jpeg',
+  '/profile-pic/266.webp',
+  '/profile-pic/266.png',
+  //icons
+  '/icons/blogger.svg',
+  '/icons/hello.svg',
+  '/icons/home.svg',
+  '/icons/suitcase.svg',
+  //styles
+  '/assets/css/style.css',
+  '/assets/css/colors-light.css',
+  '/assets/css/colors-auto.css',
+  '/assets/css/colors-dark.css',
+  //js
+  '/assets/js/animations/barba.js',
+  '/assets/js/animations/pageFirstLoad.js',
+  '/assets/js/animations/SlideOnVeiwportEnter.js',
+  '/assets/js/main.js',
+  '/assets/js/google-anylytics.js',
+  //js dependencys
+  'https://unpkg.com/@barba/core',
+  'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/ScrollTrigger.min.js',
+  'https://www.googletagmanager.com/gtag/js?id=G-5LX6PMSFGK'
+];
+
+// install event
+self.addEventListener('install', evt => {
+  //chache files
+  evt.waitUntil(
+    caches.open(chacheName).then(cache => {
+      cache.addAll(assets);
+    });
   );
+});
+
+// activate evenet
+self.addEventListener('activate', evt => {
+  
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+
 });
