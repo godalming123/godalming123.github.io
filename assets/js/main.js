51ded450f70c37343472b49763fd9c42bccc494a
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-09-02 07:41:13 -0400

    improve console messages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 //register service worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
-    .then (reg => {console.log('Service Worker registration successfull:', reg);})
-    .catch(err => {console.log('Service Worker registration failed:',      err);});
+    .then (reg => {console.info ('Service Worker registration successfull:', reg);})
+    .catch(err => {console.error('Service Worker registration failed:',      err);});
   };
