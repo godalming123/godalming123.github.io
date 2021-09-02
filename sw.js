6f439d09f23e0d21adac750d5aac3633efe0bfe0
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-09-02 11:48:58 -0400

    fix bug

--- sw.js
+++ sw.js
@@ -47,7 +47,7 @@ self.addEventListener('install', evt => {
 // activate evenet
 self.addEventListener('activate', evt => {
   evt.waitUntil(
-    chaches.keys.then(keys => {
+    caches.keys.then(keys => {
       return Promise.all(
         keys
           .filter(key => key !== chacheName)
