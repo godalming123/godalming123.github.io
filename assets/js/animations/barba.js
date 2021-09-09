1159f4282028517b44818546e4eeebe82cb53887
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-09-09 12:08:03 -0400

    remove 1st barba.init an donly use second

--- assets/js/animations/barba.js
+++ assets/js/animations/barba.js
@@ -1,32 +1,5 @@
 //import {addScrollTrigger} from './SlideOnVeiwportEnter.js';
 
-barba.init({
-    transitions: [
-      {
-        leave: (data) =>
-          gsap.to(data.current.container, {
-            opacity: 0,
-            y: 100,
-            duration: 0.4
-          }),
-        afterLeave: (data) =>
-          gsap.to(data.current.container, {
-            //hide an element so it is'nt even rendered in the veiwport
-            display: "none",
-            duration: 0
-          }),
-        enter: (data) =>
-          gsap.from(data.next.container, {
-            opacity: 0,
-            y: 100,
-            duration: 0.4
-          }),
-        //after: addScrollTrigger,//this function is from ./page first load animation see line 1
-          
-      }
-    ]
-  });  
-
   barba.init({
     transitions: [
       //home transitions
